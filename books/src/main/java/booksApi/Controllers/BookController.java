b11dce1af504b2925b29b653f9d63cf5d4023f21
“Aleksander” <aprokhorenkodev@gmail.com>
2024-10-25 18:53:39 +0200

    feat: create method to get list of book and one book

--- books/src/main/java/booksApi/Controllers/BookController.java
+++ books/src/main/java/booksApi/Controllers/BookController.java
@@ -1,6 +1,39 @@
 package booksApi.Controllers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import booksApi.Models.Book;
+
+@RestController
 public class BookController {
+
+    ArrayList<Book> data = new ArrayList<>(Arrays.asList(
+            new Book("1", "El corredor del laberinto", "James Dashner", "ISBN-135468", 50),
+            new Book("2", "Las pruebas", "James Dashner", "ISBN-5648621", 49),
+            new Book("3", "Cura Mortal", "James Dashner", "ISBN-5646523", 29)
+    ));
+
+    @GetMapping("books")
+    public ArrayList getBooks() {
+        return this.data;
+    }
+
+    @GetMapping("books/{id}")
+    public Book getMethodName(@PathVariable String id) {
+        Book select_book = null;
+
+        for (Book book : data) {
+            if (book.getId().equals(id)) {
+                select_book = new Book(book);
+            }
+        }
+        
+        return select_book;
+    }
 
 }
