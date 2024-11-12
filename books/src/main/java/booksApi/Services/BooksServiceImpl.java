f8cb291e7924d46529c84aeea1d6c4fc81ed6531
“Aleksander” <aprokhorenkodev@gmail.com>
2024-11-12 19:29:29 +0100

    feat: add methods to service class

--- books/src/main/java/booksApi/Services/BooksServiceImpl.java
+++ books/src/main/java/booksApi/Services/BooksServiceImpl.java
@@ -1,5 +1,54 @@
-package booksApi.Services;
+package booksApi.services;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import booksApi.Models.Book;
+
+@Service
 public class BooksServiceImpl {
-    
+
+    ArrayList<Book> data = new ArrayList<>(Arrays.asList(
+            new Book("1", "El corredor del laberinto", "James Dashner", "ISBN-135468", 50),
+            new Book("2", "Las pruebas", "James Dashner", "ISBN-5648621", 49),
+            new Book("3", "Cura Mortal", "James Dashner", "ISBN-5646523", 29)
+    ));
+
+
+    // Obtener todos los libros
+    public ArrayList<Book> getBooks() {
+        return this.data;
+    }
+
+    // Obtener un libro por ID
+    public Optional<Book> findBookById(String id) {
+        return data.stream()
+                .filter(book -> book.getId().equals(id))
+                .findFirst();
+    }
+
+    // Agregar un libro
+    public void addBook(Book book) {
+        data.add(book);
+    }
+
+    // Actualizar un libro
+    public Optional<Book> updateBook(String id, Book bookDetails) {
+        Optional<Book> bookToUpdate = findBookById(id);
+        bookToUpdate.ifPresent(book -> {
+            book.setName(bookDetails.getName());
+            book.setAuthor_name(bookDetails.getAuthor_name());
+            book.setIsbn(bookDetails.getIsbn());
+            book.setStock(bookDetails.getStock());
+        });
+        return bookToUpdate;
+    }
+
+    // Eliminar un libro
+    public boolean deleteBookById(String id) {
+        return data.removeIf(book -> book.getId().equals(id));
+    }
 }
