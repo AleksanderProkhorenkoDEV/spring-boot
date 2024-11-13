e0e0d65893a8f84f22568a0c526c96d88e0c1b8d
“Aleksander” <aprokhorenkodev@gmail.com>
2024-11-13 14:58:11 +0100

    feat: add all the methods that the controller need

--- books/src/main/java/booksApi/Services/BookService.java
+++ books/src/main/java/booksApi/Services/BookService.java
@@ -1,6 +1,19 @@
 package booksApi.services;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
+import booksApi.Models.Book;
+
 public interface BookService {
-    
-    
+
+    ArrayList<Book> getBooks();
+
+    Optional<Book> findBookById(String id);
+
+    void addBook(Book book);
+
+    Optional<Book> updateBook(String id, Book bookDetails);
+
+    boolean deleteBookById(String id);
 }
