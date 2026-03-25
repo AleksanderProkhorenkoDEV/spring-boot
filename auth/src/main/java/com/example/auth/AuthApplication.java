117d2ce41d4c24a3a8b76dfd277c2a77aecb2f3e
aleksander <aprokhorenkodev@gmail.com>
2026-03-25 12:35:03 +0100

    SPRING BOOT | feat: add anotation to define where is the secret

--- auth/src/main/java/com/example/auth/AuthApplication.java
+++ auth/src/main/java/com/example/auth/AuthApplication.java
@@ -2,8 +2,12 @@ package com.example.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.example.auth.services.JwtService;
 
 @SpringBootApplication
+@EnableConfigurationProperties(JwtService.class)
 public class AuthApplication {
 
 	public static void main(String[] args) {
