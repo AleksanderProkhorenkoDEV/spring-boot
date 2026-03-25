03a285771e2f23d34451885602f40135f1df7998
aleksander <aprokhorenkodev@gmail.com>
2026-03-25 12:33:48 +0100

    SPRING BOOT | feat: create a private controller, to test the security

---
+++ auth/src/main/java/com/example/auth/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.example.auth.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hola " +
+                org.springframework.security.core.context.SecurityContextHolder
+                        .getContext()
+                        .getAuthentication()
+                        .getName();
+    }
+}
