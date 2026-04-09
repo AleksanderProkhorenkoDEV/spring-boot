8627e3df2e6bda58fef2a3d2df58d175c24e2520
aleksander <aprokhorenkodev@gmail.com>
2026-04-09 08:51:51 +0200

    SPRING BOOT | feat: create the rest endpoint

---
+++ consumerservice/src/main/java/com/example/consumerservice/controller/OrderController.java
@@ -0,0 +1,30 @@
+package com.example.consumerservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.consumerservice.client.ProductClient;
+import com.example.consumerservice.dto.ProductDTO;
+
+import reactor.core.publisher.Mono;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    private final ProductClient productClient;
+
+    public OrderController(ProductClient productClient) {
+        this.productClient = productClient;
+    }
+
+    @GetMapping("/product/{id}")
+    public Mono<ProductDTO> getProductFromOrder(@PathVariable Long id)
+    {
+        return productClient.getProduct(id);
+    }
+
+}
